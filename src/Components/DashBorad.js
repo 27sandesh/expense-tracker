c3bdeac2c8849c4c118ab0b8879e9890e07270a6
27sandesh <kamblesandesh254@gmail.com>
2023-05-16 14:51:04 +0530

    adding userdata to the fitre base

--- src/Components/DashBorad.js
+++ src/Components/DashBorad.js
@@ -1,10 +1,13 @@
 import React, { useContext } from "react";
 import { Button } from "react-bootstrap";
 import DataContext from "./Store/data-context";
-
+import { useRef } from "react";
 const DashBoard = () => {
   const Authctx = useContext(DataContext);
   const token = Authctx.token;
+  const MoneyInputref = useRef();
+  const DisInputRef = useRef();
+  const CategoryInputRef = useRef();
 
   function LogoutHandler(e) {
     e.preventDefault();
@@ -39,7 +42,36 @@ const DashBoard = () => {
         console.log(error.message);
       });
   }
-
+  function submitHandler(e) {
+    e.preventDefault();
+    const EnterdMoney = MoneyInputref.current.value;
+    const EnterdDis = DisInputRef.current.value;
+    const EntredCategory = CategoryInputRef.current.value;
+    fetch(
+      "https://expense-tracker-13c13-default-rtdb.firebaseio.com/expense.json",
+      {
+        method: "POST",
+        body: JSON.stringify({
+          price: EnterdMoney,
+          discription: EnterdDis,
+          Category: EntredCategory,
+        }),
+        headers: {
+          "Content-Type": "Application/json",
+        },
+      }
+    ).then((res) => {
+      if (res.ok) {
+        res.json().then((data) => {
+          localStorage.setItem("expenses", JSON.stringify(data));
+          console.log(data);
+        });
+      } else {
+        console.log(res.error.message);
+      }
+    });
+  }
+  const expense = JSON.parse(localStorage.getItem("expenses"));
   return (
     <div>
       <section>
@@ -48,36 +80,34 @@ const DashBoard = () => {
         <Button onClick={LogoutHandler}>Logout</Button>
       </section>
       <div className="card">
-        <div className="card-body">
-          <form>
-            <div className="form-group">
-              <label htmlFor="money" className="form-control">
-                Money $:
-              </label>
-              <input type="number" className=" col-sm-2" />
-            </div>
-            <div className="form-group">
-              <label htmlFor="description" className="form-control">
-                Description:
-              </label>
-              <input type="text" className=" col-sm-2" />
-            </div>
-            <div className="form-group">
-              <label htmlFor="type" className="form-control">
-                Category:
-              </label>
-              <select className=" col-sm-2">
-                <option>Food</option>
-                <option>Movie</option>
-                <option>Dinner</option>
-              </select>
-            </div>
-            <div className="form-group">
-              {" "}
-              <Button>Add</Button>
-            </div>
-          </form>
-        </div>
+        <form onSubmit={submitHandler}>
+          <div className="form-group">
+            <label htmlFor="money" className="form-control">
+              Money $:
+            </label>
+            <input type="number" className=" col-sm-2" ref={MoneyInputref} />
+          </div>
+          <div className="form-group">
+            <label htmlFor="description" className="form-control">
+              Description:
+            </label>
+            <input type="text" className=" col-sm-2" ref={DisInputRef} />
+          </div>
+          <div className="form-group">
+            <label htmlFor="type" className="form-control">
+              Category:
+            </label>
+            <select className=" col-sm-2" ref={CategoryInputRef}>
+              <option>Food</option>
+              <option>Movie</option>
+              <option>Dinner</option>
+            </select>
+          </div>
+          <div className="form-group">
+            {" "}
+            <Button type="submit">Add</Button>
+          </div>
+        </form>
       </div>
     </div>
   );
