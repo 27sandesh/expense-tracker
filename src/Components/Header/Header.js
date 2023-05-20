2aa76be88c0fe4d93edb88ccf18f5a57ed8d0c72
27sandesh <kamblesandesh254@gmail.com>
2023-05-20 17:14:44 +0530

    email verifaction

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,7 +1,44 @@
-import { Navbar, Nav, Container } from "react-bootstrap";
+import { Navbar, Nav, Container, Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import DataContext from "../Store/data-context";
+import { useContext } from "react";
 
 const Header = () => {
+  const AUTHctx = useContext(DataContext);
+  console.log("msg", AUTHctx.token);
+  const token = AUTHctx.token;
+
+  const VerifyEmailHandler = () => {
+    if (!token) {
+      console.log("User not authenticated.");
+      return;
+    }
+
+    fetch(
+      "https://identitytoolkit.googleapis.com/v1/accounts:sendOobCode?key=AIzaSyB0dJAXTRrEtEpkORTaa2uVCWs1LHzJGgY",
+      {
+        method: "POST",
+        body: JSON.stringify({
+          requestType: "VERIFY_EMAIL",
+          idToken: token,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    )
+      .then((res) => {
+        if (res.ok) {
+          console.log("Verification email sent successfully.");
+        } else {
+          throw new Error("Error sending verification email.");
+        }
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  };
+
   return (
     <Navbar expand="lg" bg="light" variant="light">
       <Container>
@@ -16,9 +53,10 @@ const Header = () => {
           </Nav.Link>
         </Nav>
       </Container>
+      <Button onClick={VerifyEmailHandler}>Verify Email</Button>
 
       <Nav>
-        {"your profile is incomplete compelte now" && (
+        {false && (
           <Nav.Link as={Link} to="/update-profile" style={{ color: "blue" }}>
             Update Profile
           </Nav.Link>
